86bd3a94d1bfc4a7e84f81e4cf307c4c3f074eec
Trey Metrailer <TreyDevDenver@gmail.com>
2017-07-29 23:29:45 -0600

    Removed unused "site" things. Set the UserContorller as the default. Changed webroot to `/var/www/checkin`. Removed Redis.

--- checkin/protected/config/main.php
+++ checkin/protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Checkin App',
+	'defaultController' => 'user',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
